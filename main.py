421a82b668956ede84518795d670fc9d473a0685
bchachaj <benedict.chachaj@student.fairfield.edu>
2019-12-10 17:38:14 -0500

    serialize saved item dict and generate collection

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import sys
 import os
 import json
 import pickle
+import jsonpickle
 import praw
+import copy
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -23,64 +26,61 @@ saved_content_endpoint = (
 
 
 def main():
-    # access_token = reddit_request_token()
-    # saved_posts = reddit_request_data(access_token)
-    # json_res = json.dumps(saved_posts)
-    # print(len(saved_posts['data']["children"]))
-
-    # posts = saved_posts['data']["children"]
-    # print(len(posts))
-
     reddit = praw.Reddit(client_id=client_id,
                          client_secret=client_secret,
                          password=password,
                          user_agent=user_agent,
                          username=username)
-    # print(reddit.auth.url(['identity'], '...', 'permanent'))
 
     # print(dir(reddit.user))
-    user = reddit.redditor(username)
+    # user = reddit.redditor(username)
     me = reddit.user.me()
-    print(me.saved())
-    print(user.saved(limit=1000))
+    saved = me.saved
+    saved_item_collection = []
 
-    # for post in posts:
-    # print(post)
+    for saved_item in saved(limit=14):
+        duped_item = copy.deepcopy(saved_item)
 
-    # for row in json_res:
-    # print(len(row))
-    # print(row)
-    # print(json_res)
+        # delete '_reddit' attribute to save space
+        delattr(duped_item, '_reddit')
 
+        obj_dict = duped_item.__dict__
+        encoded = jsonpickle.encode(obj_dict)
 
-def reddit_request_data(access_token, data_path=None):
-    headers = {"Authorization": f"bearer {access_token}",
-               "User-Agent": user_agent}
-    fetch_url = saved_content_endpoint
+        saved_item_collection.append(encoded)
 
-    if data_path == None:
-        fetch_url = saved_content_endpoint
-    else:
-        fetch_url = data_path
-
-    data = requests.get(fetch_url, headers=headers)
-    if(data.status_code == 200):
-        return data.json()
-    else:
-        sys.exit(data.status_code)
+    print(len(saved_item_collection))
 
 
-def reddit_request_token():
-    client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-    post_data = {"grant_type": "password",
-                 "username":  username, "password": password}
-    headers = {"User-Agent": user_agent}
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=client_auth, data=post_data, headers=headers)
-    res_json = response.json()
+# def reddit_request_data(access_token, data_path=None):
+#     headers = {"Authorization": f"bearer {access_token}",
+#                "User-Agent": user_agent}
+#     fetch_url = saved_content_endpoint
 
-    return res_json["access_token"]
+#     if data_path == None:
+#         fetch_url = saved_content_endpoint
+#     else:
+#         fetch_url = data_path
+
+#     data = requests.get(fetch_url, headers=headers)
+#     if(data.status_code == 200):
+#         return data.json()
+#     else:
+#         sys.exit(data.status_code)
+
+
+# def reddit_request_token():
+#     client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+#     post_data = {"grant_type": "password",
+#                  "username":  username, "password": password}
+#     headers = {"User-Agent": user_agent}
+#     response = requests.post("https://www.reddit.com/api/v1/access_token",
+#                              auth=client_auth, data=post_data, headers=headers)
+#     res_json = response.json()
+
+#     return res_json["access_token"]
 
 
 if __name__ == '__main__':
     sys.exit(main())
+#
