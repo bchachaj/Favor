0bbc0e73c8ab74c305eb3058c1d336965e51348f
bchachaj <benedict.chachaj@student.fairfield.edu>
2020-01-20 14:46:54 -0500

    expansion panel styling

--- favor_client/src/components/pages/ContentDisplayPage.js
+++ favor_client/src/components/pages/ContentDisplayPage.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SettingsIcon from '@material-ui/icons/Settings';
 import Divider from '@material-ui/core/Divider';
-
 import Navbar from '../shared/navbar/Navbar';
 import ItemIndex from '../shared/ItemIndex';
 import SubChipIndex from '../SubChipIndex';
 import TypeFilterControl from '../TypeFilterControl';
 import useChipFilter from '../../hooks/useChipFilter';
 
-
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     root: {
         padding: "8px 4px 24px"
     },
 }));
+
+const ExpansionPanelSummary = withStyles({
+    content: {
+        '& > *': {
+            marginRight: '12px',
+        },
+    },
+    expanded: {},
+})(MuiExpansionPanelSummary);
 
 export default function ContentDisplayPage({ savedItems, subreddits }) {
     const classes = useStyles();
@@ -38,12 +46,12 @@ export default function ContentDisplayPage({ savedItems, subreddits }) {
         const filteredBySub = chipFilteredState();
 
         const filteredByType = filteredBySub.filter((item) => {
-            const filterByComment = item.hasOwnProperty('body') && showComments; 
-            const filterBySub = !item.hasOwnProperty('body') && showSubs; 
-            
-            if(filterByComment) return item; 
-            if(filterBySub) return item; 
-        }); 
+            const filterByComment = item.hasOwnProperty('body') && showComments;
+            const filterBySub = !item.hasOwnProperty('body') && showSubs;
+
+            if (filterByComment) return item;
+            if (filterBySub) return item;
+        });
 
         return filteredByType;
     };
@@ -55,19 +63,18 @@ export default function ContentDisplayPage({ savedItems, subreddits }) {
             <ExpansionPanel defaultExpanded>
                 <ExpansionPanelSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1c-content"
-                    id="panel1c-header"
                 >
-                <Typography>Filters</Typography>
-                  
+                    <SettingsIcon />
+                    <Typography>Filters</Typography>
+
                 </ExpansionPanelSummary>
-                    <Divider />
+                <Divider />
                 <ExpansionPanelDetails className={classes.root}>
 
                     <SubChipIndex subreddits={subreddits}
                         toggleChipFilter={toggleChipFilter}
                         filters={filters} />
-           
+
                 </ExpansionPanelDetails>
                 <ExpansionPanelDetails>
                     <TypeFilterControl toggleComments={toggleComments}
@@ -75,9 +82,9 @@ export default function ContentDisplayPage({ savedItems, subreddits }) {
                         showComments={showComments}
                         showSubs={showSubs} />
                 </ExpansionPanelDetails>
-       
-            </ExpansionPanel>           
-            <ItemIndex items={filteredState()}/>
+
+            </ExpansionPanel>
+            <ItemIndex items={filteredState()} />
         </>
     )
 }
